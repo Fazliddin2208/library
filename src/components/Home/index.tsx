a778cb54f53207d0d9ec87f4b652b9f4dd44b4aa
Fazliddin2208 <fazliddinabdurrahmon@gmail.com>
2025-02-11 16:06:07 +0500

    quotes

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,16 +1,11 @@
-import Carousel from "react-multi-carousel";
 import QuotesSection from "./sections/QuotesSection";
 
 export default function HomeComponent() {
-  
-
   return (
     <div className="grid grid-cols-8 gap-10">
       <QuotesSection />
 
-      <div className="col-span-5 border">
-        salom
-      </div>
+      <div className="col-span-5 border">salom</div>
     </div>
   );
 }
