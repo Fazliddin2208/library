0dac9a948c159cb2ab5997f0991eaa36eaabfa10
Fazliddin2208 <fazliddinabdurrahmon@gmail.com>
2025-02-19 14:24:20 +0500

    skeleton added to arrivals

--- src/components/Home/ui/Arrivals/ArrivalCards.tsx
+++ src/components/Home/ui/Arrivals/ArrivalCards.tsx
@@ -1,18 +1,29 @@
 import {useNewBooksList} from "@/services/book/queries";
 import ArrivalCard from "./ArrivalCard";
 import {BookType} from "@/types/BookType";
+import {Skeleton} from "@/components/ui/skeleton";
 
 type Props = {};
 
 export default function ArrivalCards({}: Props) {
-  const {books, isPending, isError} = useNewBooksList();
-  console.log(books, isPending, isError);
+  const {books, isPending} = useNewBooksList();
 
   return (
-    <div className="bg-light flex-1 rounded-r-[10px] grid grid-cols-4 py-3 px-4 gap-5">
-      {books?.map((book: BookType) => (
-        <ArrivalCard book={book} key={book?._id} />
-      ))}
-    </div>
+    <>
+      {isPending ? (
+        <div className="bg-light flex-1 rounded-r-[10px] grid grid-cols-4 py-3 px-4 gap-5">
+          <Skeleton />
+          <Skeleton />
+          <Skeleton />
+          <Skeleton />
+        </div>
+      ) : (
+        <div className="bg-light flex-1 rounded-r-[10px] grid grid-cols-4 py-3 px-4 gap-5">
+          {books?.map((book: BookType) => (
+            <ArrivalCard book={book} key={book?._id} />
+          ))}
+        </div>
+      )}
+    </>
   );
 }
