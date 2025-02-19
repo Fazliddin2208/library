eae7d8af95e6ac832c98fb7f949fbce71b658c64
Fazliddin2208 <fazliddinabdurrahmon@gmail.com>
2025-02-19 16:36:59 +0500

    recommended

--- src/components/Home/ui/Arrivals/ArrivalCards.tsx
+++ src/components/Home/ui/Arrivals/ArrivalCards.tsx
@@ -11,7 +11,7 @@ export default function ArrivalCards({}: Props) {
   return (
     <div className="bg-light flex-1 rounded-r-[10px] grid grid-cols-4 py-3 px-4 gap-5">
       {isPending
-        ? Array.from({length: 4}).map((_, index) => <Skeleton key={index}  />)
+        ? Array.from({length: 4}).map((_, index) => <Skeleton key={index} />)
         : books?.map((book: BookType) => <ArrivalCard book={book} key={book?._id} />)}
     </div>
   );
