c934d235efe2ed4c617f793d9d9d7838189818ad
Fazliddin2208 <fazliddinabdurrahmon@gmail.com>
2025-02-26 18:37:00 +0500

    channges

--- src/components/BookView/ui/Info/TitleComponent.tsx
+++ src/components/BookView/ui/Info/TitleComponent.tsx
@@ -3,13 +3,14 @@ import {RatingGroup} from "@chakra-ui/react";
 
 export default function TitleComponent() {
   const {book} = useBookViewContext();
+  const rating = book?.price ? book?.price / 2 : 0;
+
   return (
     <div>
       <h1>{book?.title}</h1>
       <p>By {book?.author}</p>
 
-      <RatingGroup.Root readOnly count={5} defaultValue={3} size="sm">
-        <RatingGroup.HiddenInput />
+      <RatingGroup.Root count={5} value={rating} size="sm" colorPalette={"yellow"} readOnly allowHalf>
         <RatingGroup.Control>
           {Array.from({length: 5}).map((_, index) => (
             <RatingGroup.Item key={index} index={index + 1}>
