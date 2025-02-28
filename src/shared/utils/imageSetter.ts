c7e532c30cc95f7d61a1331dc45c121a3baae5e5
Fazliddin2208 <fazliddinabdurrahmon@gmail.com>
2025-02-28 10:52:37 +0500

    type for imageSetter util

--- src/shared/utils/imageSetter.ts
+++ src/shared/utils/imageSetter.ts
@@ -1,5 +1,5 @@
 import DefaultImage from "@/assets/images/book.jpg";
 
-export const imageSetter = (image?: string) => {
+export const imageSetter = (image?: string): string => {
   return image || DefaultImage;
 };
